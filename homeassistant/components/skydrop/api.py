d9e838def0715d42854aa2bec70a6734dc6db1e7
Jordan Bean <jordan.bean@gmail.com>
2020-04-12 17:24:22 -0500

    Import skydroppy

--- homeassistant/components/skydrop/api.py
+++ homeassistant/components/skydrop/api.py
@@ -1,18 +1,13 @@
 """API for Skydrop bound to Home Assistant OAuth."""
 from asyncio import run_coroutine_threadsafe
 
-from aiohttp import ClientSession
-import my_pypi_package
+import skydroppy
 
 from homeassistant import config_entries, core
 from homeassistant.helpers import config_entry_oauth2_flow
 
-# TODO the following two API examples are based on our suggested best practices
-# for libraries using OAuth2 with requests or aiohttp. Delete the one you won't use.
-# For more info see the docs at <insert url>.
 
-
-class ConfigEntryAuth(my_pypi_package.AbstractAuth):
+class ConfigEntryAuth(skydroppy.SkydropClient):
     """Provide Skydrop authentication tied to an OAuth2 based config entry."""
 
     def __init__(
@@ -36,23 +31,3 @@ class ConfigEntryAuth(my_pypi_package.AbstractAuth):
         ).result()
 
         return self.session.token
-
-
-class AsyncConfigEntryAuth(my_pypi_package.AbstractAuth):
-    """Provide Skydrop authentication tied to an OAuth2 based config entry."""
-
-    def __init__(
-        self,
-        websession: ClientSession,
-        oauth_session: config_entry_oauth2_flow.OAuth2Session,
-    ):
-        """Initialize Skydrop auth."""
-        super().__init__(websession)
-        self._oauth_session = oauth_session
-
-    async def async_get_access_token(self):
-        """Return a valid access token."""
-        if not self._oauth_session.is_valid:
-            await self._oauth_session.async_ensure_token_valid()
-
-        return self._oauth_session.token
