e4ece27a69f85814b7fa7ca9afb5987f3de5e0e9
Jordan Bean <jordan.bean@gmail.com>
2020-04-12 21:34:07 -0500

    Refactored form, login stub

--- homeassistant/components/skydrop/config_flow.py
+++ homeassistant/components/skydrop/config_flow.py
@@ -1,12 +1,16 @@
 """Config flow for Skydrop."""
 import logging
 
+from skydroppy import SkydropClient
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
+
+DOCS_URL = "https://www.home-assistant.io/integrations/skydrop"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,22 +20,61 @@ class OAuth2FlowHandler(
 ):
     """Config flow to handle Skydrop OAuth2 authentication."""
 
+    VERSION = 1
+
     DOMAIN = DOMAIN
-    # TODO Pick one from config_entries.CONN_CLASS_*
-    CONNECTION_CLASS = config_entries.CONN_CLASS_UNKNOWN
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    def __init__(self):
+        """Initialize flow."""
+        self._username = vol.UNDEFINED
+        self._password = vol.UNDEFINED
 
     async def async_step_user(self, user_input=None):
         """Configure user login page."""
+        errors = {}
 
-        # Specify items in the order they are to be displayed in the UI
-        data_schema = {
-            vol.Required("username"): str,
-            vol.Required("password"): str,
-        }
+        if self._async_current_entries():
+            return self.async_abort(reason="already_configured")
 
-        return self.async_show_form(step_id="init", data_schema=vol.Schema(data_schema))
+        if user_input is not None:
+            self._username = user_input["username"]
+            self._password = user_input["password"]
+
+            error = await self.hass.async_add_executor_job(
+                self.try_login, self._username, self._password
+            )
+
+            if error:
+                errors["base"] = error
+            else:
+                return self.async_create_entry(
+                    title=user_input[CONF_USERNAME],
+                    data=user_input,
+                    description_placeholders={"docs_url": DOCS_URL},
+                )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
+            ),
+            description_placeholders={"docs_url": DOCS_URL},
+            errors=errors,
+        )
 
     @property
     def logger(self) -> logging.Logger:
         """Return logger."""
         return logging.getLogger(__name__)
+
+    @staticmethod
+    def try_login(username, password):
+        """Try logging in to device and return any errors."""
+
+        try:
+            SkydropClient(username, password)
+        except SkydropClient.Unauthorized:
+            return "Unauthorized"
+
+        return None
